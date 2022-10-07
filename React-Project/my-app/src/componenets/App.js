98bec1128ac4c313074954f8a1b27de66341d081
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-10-07 15:37:47 +0300

    Add

--- React-Project/my-app/src/componenets/App.js
+++ React-Project/my-app/src/componenets/App.js
@@ -2,39 +2,39 @@ import React, { Component } from 'react';
 //import './App.css';
 import Form from "./Form";
 import Recipes from "./Recipes";
-class App extends Component {
-state = {
-    recipes: []
-}
-getRecipe = async (e) => {
-    const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault();
-    const api_call = await fetch(`https://recipe-app-an.herokuapp.com/recipe`);
+// class App extends Component {
+// state = {
+//     recipes: []
+// }
+// getRecipe = async (e) => {
+//     const recipeName = e.target.elements.recipeName.value;
+//     e.preventDefault();
+//     const api_call = await fetch(`https://recipe-app-an.herokuapp.com/recipe`);
 
-    const data = await api_call.json();
-    this.setState({ recipes: data.recipes });
-    console.log(this.state.recipes);
-}
-componentDidMount = () => {
-    const json = localStorage.getItem("recipes");
-    const recipes = JSON.parse(json);
-    this.setState({ recipes });
-}
-componentDidUpdate = () => {
-    const recipes = JSON.stringify(this.state.recipes);
-    localStorage.setItem("recipes", recipes);
-}
-render() {
-    return (
-    <div className="App">
-        <header className="App-header">
-        <h1 className="App-title">Recipe Search</h1>
-        </header>
-        <Form getRecipe={this.getRecipe} />
-        <Recipes recipes={this.state.recipes} />
-    </div>
-    );
-}
-}
+//     const data = await api_call.json();
+//     this.setState({ recipes: data.recipes });
+//     console.log(this.state.recipes);
+// }
+// componentDidMount = () => {
+//     const json = localStorage.getItem("recipes");
+//     const recipes = JSON.parse(json);
+//     this.setState({ recipes });
+// }
+// componentDidUpdate = () => {
+//     const recipes = JSON.stringify(this.state.recipes);
+//     localStorage.setItem("recipes", recipes);
+// }
+// render() {
+//     return (
+//     <div className="App">
+//         <header className="App-header">
+//         <h1 className="App-title">Recipe Search</h1>
+//         </header>
+//         <Form getRecipe={this.getRecipe} />
+//         <Recipes recipes={this.state.recipes} />
+//     </div>
+//     );
+// }
+// }
 
-export default App;
+// export default App;
