57ea6407fdc1be5088338422c82c4fa04b3100a0
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-10-07 08:28:44 +0300

    Add

--- my-app/src/componenets/Router.js
+++ my-app/src/componenets/Router.js
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
-// import {Switch } from 'react-router-dom';
-// import { Route } from 'react-router-dom';
-// import App from './App';
-// import Recipe from "./Recipe";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import App from './App';
+import Recipe from "./Recipe";
 
 // const Router = () => (
 // <BrowserRouter>
