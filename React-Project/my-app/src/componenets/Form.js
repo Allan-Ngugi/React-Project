82b89a7b32b8f380e1688624505d53337e9680a8
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-10-07 15:19:25 +0300

    Add

---
+++ React-Project/my-app/src/componenets/Form.js
@@ -0,0 +1,10 @@
+ import React from 'react';
+
+const Form = props => (
+	<form onSubmit={props.getRecipe} style={{ marginBottom:"2rem" }}>
+		<input className="form_input" type="text" nname="recipeName"/>
+		<button className="form_button">Search</button>
+	</form>
+);
+
+export default Form;
