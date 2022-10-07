b6fa883952d800c9f1c338e5c07948efa46d7e38
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-10-07 08:26:47 +0300

    Add

--- my-app/src/componenets/App.js
+++ my-app/src/componenets/App.js
@@ -14,30 +14,30 @@ getRecipe = async (e) => {
     e.preventDefault();
     const api_call = await fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=chicken`);
     
-//     const data = await api_call.json();
-//     this.setState({ recipes: data.recipes });
-//     console.log(this.state.recipes);
-// }
-// componentDidMount = () => {
-//     const json = localStorage.getItem("recipes");
-//     const recipes = JSON.parse(json);
-//     this.setState({ recipes });
-// }
-// componentDidUpdate = () => {
-//     const recipes = JSON.stringify(this.state.recipes);
-//     localStorage.setItem("recipes", recipes);
-// }
-// render() {
-//     return (
-//     <div className="App">
-//         <header className="App-header">
-//     <h1 className="App-title">Recipe Search</h1>
-//         </header>
-//         <Form getRecipe={this.getRecipe} />
-//         <Recipes recipes={this.state.recipes} />
-// </div>
-//     );
-// }
-// }
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+}
+componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+}
+componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+}
+render() {
+    return (
+    <div className="App">
+        <header className="App-header">
+    <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+</div>
+    );
+}
+}
 
-// export default App;
+export default App;
