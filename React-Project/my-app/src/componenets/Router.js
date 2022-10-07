2973639a60be684c1a18a8fcad1d3ad22562d185
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-10-07 15:39:31 +0300

    Add

--- React-Project/my-app/src/componenets/Router.js
+++ React-Project/my-app/src/componenets/Router.js
@@ -5,13 +5,13 @@ import { Route } from 'react-router-dom';
 import App from './App';
 import Recipe from "./Recipe";
 
-const Router = () => (
-<BrowserRouter>
-    <Switch>
-    <Route path="/" component={App} exact />
-    <Route path="/recipe/:id" component={Recipe} />
-    </Switch>
-</BrowserRouter>
-);
+// const Router = () => (
+// <BrowserRouter>
+//     <Switch>
+//     <Route path="/" component={App} exact />
+//     <Route path="/recipe/:id" component={Recipe} />
+//     </Switch>
+// </BrowserRouter>
+// );
 
-export default Router;
+// export default Router;
